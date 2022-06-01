0d43cb4f8e2bc463c1f93fb203a5266e6b400e7d
loomstyla <pabloferrari.hh@gmail.com>
2022-05-31 21:07:16 -0300

    Unity Project

---
+++ Assets/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController _characterController;
+    public float _moveSpeed = 50f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 moveDir = transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal");
+       
+        _characterController.SimpleMove(moveDir * _moveSpeed*Time.deltaTime);
+    }
+}
